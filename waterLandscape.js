b602c946d35c92de41196396c5931ff788cabcc7
Colin Parsons <casparsons@gmail.com>
2016-10-13 10:01:28 +0800

    add ascii art output

--- waterLandscape.js
+++ waterLandscape.js
@@ -2,16 +2,15 @@
 // represented as an array of integers.
 // each integer represents the height of a column.
 // for a given landscape which is flooded with water,
-// first find the resulting water level,
-// then find the "area" of water retained by the landscape
+// find the "area" of water retained by the landscape
 // (assuming each block is 1x1 unit).
-// finally, create a visual representation of the flooded landscape.
+// create a visual representation of the flooded landscape.
 
-const landscape = generateLandscape(10, 10);
-console.log(landscape);
+const width = 20;
+const maxHeight = 10;
+
+const landscape = generateLandscape(width, maxHeight);
 simulateFlood(landscape);
-
-// TODO: draw flooded landscape as ascii art
 
 function generateLandscape(width, maxHeight) {
   const result = [];
@@ -41,14 +40,11 @@ function simulateFlood(landscape) {
       max = landscape[i];
     }
   }
-
   maxRight.reverse();
 
   const maxBoth = maxLeft.map((item, i) => {
     return Math.min(item, maxRight[i]);
   });
-
-  const waterLevel = Math.max.apply(null, maxBoth);
 
   const totalArea = maxBoth.reduce((sum, item, i) => {
     if (landscape[i] < item) {
@@ -58,7 +54,23 @@ function simulateFlood(landscape) {
     }
   }, 0);
 
-  console.log(`water level: ${waterLevel}`);
+  // draw flooded landscape as ascii art
+  for (var y = maxHeight; y > 0; y--) {
+    var row = '';
+    for (var x = 0; x < width; x++) {
+      if (landscape[x] >= y) {
+        row += '[#]';
+      } else if (maxBoth[x] >= y) {
+        row += '~~~'
+      } else {
+        row += '   ';
+      }
+
+    }
+    console.log(row);
+  }
+
+  console.log('\n');
   console.log(`total water area: ${totalArea} square units`);
 
 }
